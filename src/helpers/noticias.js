9ec4758b637de1e998a7a44a9abdd2f2475ba959
Jeison Ferrer <jeison120120@hotmail.com>
2025-12-23 18:08:00 -0500

    animando secciones

---
+++ src/helpers/noticias.js
@@ -0,0 +1,38 @@
+export const noticias = [
+    {
+        fecha: "2025-12-23",
+        titulo: "Participando de ponencia sobre la seguridad en la Región",
+        descripcion: "Descripcion de la noticia 1",
+        imagen: "https://i.ytimg.com/vi/4jPm1abBziI/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLAFF-FiqwQPyhaEYkPof64YTGdcLg",
+    },
+    {
+        fecha: "2025-12-23",
+        titulo: "Noticia 2",
+        descripcion: "Descripcion de la noticia 2",
+        imagen: "https://pbs.twimg.com/profile_images/1512556072556449802/mkQswIxg_400x400.jpg",
+    },
+    {
+        fecha: "2025-12-23",
+        titulo: "Noticia 3",
+        descripcion: "Descripcion de la noticia 3",
+        imagen: "https://pbs.twimg.com/profile_images/1512556072556449802/mkQswIxg_400x400.jpg",
+    },
+    {
+        fecha: "2025-12-23",
+        titulo: "Noticia 4",
+        descripcion: "Descripcion de la noticia 4",
+        imagen: "https://pbs.twimg.com/profile_images/1512556072556449802/mkQswIxg_400x400.jpg",
+    },
+    {
+        fecha: "2025-12-23",
+        titulo: "Noticia 5",
+        descripcion: "Descripcion de la noticia 5",
+        imagen: "https://pbs.twimg.com/profile_images/1512556072556449802/mkQswIxg_400x400.jpg",
+    },
+    {
+        fecha: "2025-12-23",
+        titulo: "Noticia 6",
+        descripcion: "Descripcion de la noticia 6",
+        imagen: "https://pbs.twimg.com/profile_images/1512556072556449802/mkQswIxg_400x400.jpg",
+    },
+];
