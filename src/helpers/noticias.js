c8e37bf5d5e144ef0d979594984616641c2bfab1
Jeison Ferrer <jeison120120@hotmail.com>
2026-01-10 13:15:44 -0500

    Corrección de la importación de la imagenes para los noticias

--- src/helpers/noticias.js
+++ src/helpers/noticias.js
@@ -1,44 +1,51 @@
+import S1 from "../assets/fotos/noticias/S1.png";
+import S2 from "../assets/fotos/noticias/S2.png";
+import S3 from "../assets/fotos/noticias/S3.png";
+import S4 from "../assets/fotos/noticias/S4.png";
+import S5 from "../assets/fotos/noticias/S5.png";
+import S6 from "../assets/fotos/noticias/S6.png";
+
 export const noticias = [
   {
     fecha: "2025-12-27",
     link: "https://web.facebook.com/reel/1505387540542086/?s=single_unit",
     titulo: "Norte de Santander necesita una voz firme que lo defienda.",
     descripcion: "",
-    imagen: "src/assets/fotos/noticias/S1.png",
+    imagen: S1,
   },
   {
     fecha: "2025-12-21",
     link: "https://www.instagram.com/p/DSfL6ZnDmdJ/",
     titulo: "¿Y la inversión dónde está? El país va a completar 4 años...",
     descripcion: "",
-    imagen: "src/assets/fotos/noticias/S2.png",
+    imagen: S2,
   },
   {
     fecha: "2025-12-29",
     titulo: "Más de 11 meses de enfrentamientos entre ELN...",
     link: "https://web.facebook.com/reel/880667747750762",
     descripcion: "",
-    imagen: "src/assets/fotos/noticias/S3.png",
+    imagen: S3,
   },
   {
     fecha: "2025-12-22",
     link: "https://www.instagram.com/p/DSntr4FEnvv/",
     titulo: "El presidente Gustavo Petro ataca a la prensa...",
     descripcion: "",
-    imagen: "src/assets/fotos/noticias/S4.png",
+    imagen: S4,
   },
   {
     fecha: "2025-12-31",
     link: "https://web.facebook.com/reel/1121219999929942/?s=single_unit",
     titulo: "El salario mínimo sube, pero no todos ganan.",
     descripcion: "",
-    imagen: "src/assets/fotos/noticias/S5.png",
+    imagen: S5,
   },
   {
     fecha: "2026-01-05",
     link: "https://www.instagram.com/p/DTEHWLjEkff/",
     titulo: "Esto es lo que nos espera en Norte de Santander.",
     descripcion: "",
-    imagen: "src/assets/fotos/noticias/S6.png",
+    imagen: S6,
   },
 ];
