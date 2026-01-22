605f36ceef92b6f2a5d5e9386233eb63028ff21a
GuillermoGU24 <jguillermogu24@gmail.com>
2026-01-21 23:56:22 -0500

    add nueva noticia

--- src/helpers/noticias.js
+++ src/helpers/noticias.js
@@ -1,6 +1,7 @@
 import n1 from "../assets/fotos/noticias/n1.webp";
 import n2 from "../assets/fotos/noticias/n2.webp";
 import n3 from "../assets/fotos/noticias/n3.png";
+import n4 from "../assets/fotos/noticias/NoticiaNorteActivo.png";
 
 export const noticias = [
   {
@@ -26,5 +27,13 @@ export const noticias = [
     link: "https://elpais.com/america-colombia/2023-09-07/la-directora-del-periodico-la-opinion-de-cucuta-es-amenazada-de-muerte.html",
     descripcion: "",
     imagen: n3,
+  },
+  {
+    fecha: "2026-01-21",
+    titulo:
+      "La periodista Estefanía Colmenares Hernández ex directora del diario La Opinión busca llegar al senado con el Centro Democrático.",
+    link: "https://web.facebook.com/share/v/1CaKrT7edq/",
+    descripcion: "",
+    imagen: n4,
   },
 ];
